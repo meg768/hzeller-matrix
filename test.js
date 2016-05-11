e1f419ccb2bf30e7faa3a9f4fcf3b100b72df2d0
meg768 <magnus@egelberg.se>
2016-05-11 22:31:43 +0200

    -

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 var matrix = require('./build/Release/matrix');
 
-function draw() {
+function draw(red, green, blue) {
 
 	for (var x = 0; x < 32; x++)
 		for (var y = 0; y < 32; y++)
-			matrix.setPixel(x, y, 255, 255, 255);
+			matrix.setPixel(x, y, red, green, blue);
 	
 	matrix.refresh();
 }
 
 matrix.start();
-draw();
+draw(255, 255, 255);
+draw(255, 0, 0);
+draw(0, 255, 0);
+draw(0, 0, 255);
 
 
 setTimeout(function(){
